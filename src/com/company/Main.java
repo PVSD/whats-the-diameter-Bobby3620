2af8b74914e51c43c14dc5011612e5a006366c7f
rd568 <bobbyday3620@gmail.com>
2018-10-05 11:17:11 -0400

    ayayyyyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Circle cir1 = new Circle(35.5);
-        System.out.println(cir1.Diameter());
+        Circle cir = new Circle(35.5);
+        System.out.println(cir.Diameter());
 
     }
 }
